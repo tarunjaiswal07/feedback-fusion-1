1d25a66b867007c47e9e52907fbe3ba8e150cba2
Tarun jaiswal <tarunjaiswal7890@gmail.com>
2026-04-14 10:22:29 +0530

    feat: initial commit

---
+++ declaration.d.ts
@@ -0,0 +1,3 @@
+//we use this file to declare modules for non-TS files, such as CSS files. This allows us to import CSS files in our TypeScript code without getting type errors.   
+
+declare module "*.css"; 
